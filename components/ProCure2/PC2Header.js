952cd5e86765c83d97ebf7d6b1a7dc49281c0872
NotWhoYoureThinkingOf <acurtis1286@gmail.com>
2022-04-08 13:39:03 -0400

    PC2 active header menu button will be green

--- components/ProCure2/PC2Header.js
+++ components/ProCure2/PC2Header.js
@@ -14,29 +14,47 @@ const PC2Header = () => {
     proCure2LanguageScreenState
   );
 
+  console.log("menu", menu);
+
   return (
     !languageScreen && (
       <div className="ProCure2-Header flex gap-2">
         <div
-          className="flex-[0.25] flex justify-center bg-gradient-to-b from-[#6B6A6A] to-[#3D3D3D] py-3 rounded-b-lg cursor-pointer transition hover:brightness-150"
+          className={`flex-[0.25] flex justify-center bg-gradient-to-b ${
+            menu !== "main"
+              ? "from-[#6B6A6A] to-[#3D3D3D]"
+              : "from-[#2BC25F] to-[#1B7D3D]"
+          }  py-3 rounded-b-lg cursor-pointer transition hover:brightness-125`}
           onClick={() => setMenu("main")}
         >
           <HomeOutlined style={{ fontSize: "3rem" }} />
         </div>
         <div
-          className="flex-[0.25] flex justify-center bg-gradient-to-b from-[#6B6A6A] to-[#3D3D3D] py-3 rounded-b-lg cursor-pointer transition hover:brightness-150"
+          className={`flex-[0.25] flex justify-center bg-gradient-to-b ${
+            menu !== "settings"
+              ? "from-[#6B6A6A] to-[#3D3D3D]"
+              : "from-[#2BC25F] to-[#1B7D3D]"
+          }  py-3 rounded-b-lg cursor-pointer transition hover:brightness-125`}
           onClick={() => setMenu("settings")}
         >
           <SettingsOutlined style={{ fontSize: "3rem" }} />
         </div>
         <div
-          className="flex-[0.25] flex justify-center bg-gradient-to-b from-[#6B6A6A] to-[#3D3D3D] py-3 rounded-b-lg cursor-pointer transition hover:brightness-150"
+          className={`flex-[0.25] flex justify-center bg-gradient-to-b ${
+            menu !== "wifi"
+              ? "from-[#6B6A6A] to-[#3D3D3D]"
+              : "from-[#2BC25F] to-[#1B7D3D]"
+          }  py-3 rounded-b-lg cursor-pointer transition hover:brightness-125`}
           onClick={() => setMenu("wifi")}
         >
           <Wifi style={{ fontSize: "3rem" }} />
         </div>
         <div
-          className="flex-[0.25] flex justify-center bg-gradient-to-b from-[#6B6A6A] to-[#3D3D3D] py-3 rounded-b-lg cursor-pointer transition hover:brightness-150"
+          className={`flex-[0.25] flex justify-center bg-gradient-to-b ${
+            menu !== "account"
+              ? "from-[#6B6A6A] to-[#3D3D3D]"
+              : "from-[#2BC25F] to-[#1B7D3D]"
+          }  py-3 rounded-b-lg cursor-pointer transition hover:brightness-125`}
           onClick={() => setMenu("account")}
         >
           <PersonOutlineOutlined style={{ fontSize: "3rem" }} />

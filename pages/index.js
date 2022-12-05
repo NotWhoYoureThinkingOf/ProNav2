a8bc6ed8e56f211542635d54b35553e7f0aa904c
Adam <lionheart1286@hotmail.com>
2022-12-05 13:19:07 -0500

    testing some SEO

--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,14 @@ export default function Home() {
       <Head>
         <title>ProNav Guide</title>
         <link rel="icon" href="/sprintray-icon.png" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:site_name" content="https://pro-nav2.vercel.app/" />
+        <meta
+          property="og:description"
+          content="An interactive guide to learning how navigate and use SprintRay products"
+        />
+        <meta property="og:image" content="/sprintray-icon.png" />
       </Head>
 
       <MainScreen />

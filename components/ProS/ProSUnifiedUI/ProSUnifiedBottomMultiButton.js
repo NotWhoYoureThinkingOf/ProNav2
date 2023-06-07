94d866c166433d17a3e310fabae8b219196ce20e
Adam <lionheart1286@hotmail.com>
2023-06-07 16:28:45 -0400

    added print complete screen and home button

--- components/ProS/ProSUnifiedUI/ProSUnifiedBottomMultiButton.js
+++ components/ProS/ProSUnifiedUI/ProSUnifiedBottomMultiButton.js
@@ -1,9 +1,20 @@
 import React from "react";
 
-export const ProSUnifiedBottomMultiButton = ({ text }) => {
+export const ProSUnifiedBottomMultiButton = ({
+  text,
+  bg,
+  textSize,
+  Text,
+  Icon,
+  img,
+}) => {
   return (
-    <div className="ProSUnifiedBottomMultiButton bg-[#535455] rounded-t-md flex justify-center items-center h-[80px] absolute bottom-0 w-full cursor-pointer hover:brightness-125">
-      <p className="text-[24px]">{text}</p>
+    <div
+      className="ProSUnifiedBottomMultiButton bg-[#535455] rounded-t-md flex justify-center items-center h-[80px] absolute bottom-0 w-full cursor-pointer hover:brightness-125"
+      style={{ background: bg }}
+    >
+      {Text && <p style={{ fontSize: textSize }}>{text}</p>}
+      {Icon && <div>{img}</div>}
     </div>
   );
 };
--- components/ProS/ProSUnifiedUI/ProSUnifiedPrintComplete.js
+++ components/ProS/ProSUnifiedUI/ProSUnifiedPrintComplete.js
@@ -1,7 +1,52 @@
 import React from "react";
+import { useRecoilState } from "recoil";
+import { proSUnifiedNavState } from "../../../atoms/proSUnifiedNavAtom";
+import { ProSUnifiedScreenHeader } from "./ProSUnifiedScreenHeader";
+import { ThumbDown, ThumbUp } from "@material-ui/icons";
+import { ProSUnifiedBottomMultiButton } from "./ProSUnifiedBottomMultiButton";
 
 export const ProSUnifiedPrintComplete = () => {
+  const [unifiedMenu, setUnifiedMenu] = useRecoilState(proSUnifiedNavState);
   return (
-    <div className="ProSUnifiedPrintComplete">Print Complete/Feedback</div>
+    <div className="ProSUnifiedPrintComplete h-full">
+      <ProSUnifiedScreenHeader title="Print Job Name" />
+      <div className="CompleteContent pt-10 pl-6 h-[710px] flex flex-col">
+        <div className="CompleteText flex flex-col items-center flex-1 justify-center pb-44">
+          <h3 className="text-[40px]" style={{ fontWeight: 600 }}>
+            Print Job Finished
+          </h3>
+          <p className="text-[24px] opacity-[65%]" style={{ fontWeight: 300 }}>
+            Are you satisfied with the results of this print job?
+          </p>
+        </div>
+        <div className="CompleteButtons flex gap-[.6rem]">
+          <div className="PrintingCancel relative flex-[.33]">
+            <ProSUnifiedBottomMultiButton
+              Icon
+              img={<ThumbDown style={{ fontSize: "1.8rem" }} />}
+              bg="#EE6622"
+            />
+          </div>
+          <div
+            className="PrintingViewLayer relative flex-[.33]"
+            onClick={() => setUnifiedMenu("unifiedPrint")}
+          >
+            <ProSUnifiedBottomMultiButton
+              Text
+              text="HOME"
+              textSize="24px"
+              bg="#303132"
+            />
+          </div>
+          <div className="PrintingPause relative flex-[.33]">
+            <ProSUnifiedBottomMultiButton
+              Icon
+              img={<ThumbUp style={{ fontSize: "1.8rem" }} />}
+              bg="#66AA22"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   );
 };
